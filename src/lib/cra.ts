4cadda84d428756c63c05ce7d6a80c763aac45fb
Harsh Vardhan Gautam <hvgautam24.vnit@gmail.com>
2021-06-26 18:17:53 +0530

    ADD Error Handling when wrong project-name is passed

--- src/lib/cra.ts
+++ src/lib/cra.ts
@@ -3,15 +3,26 @@ import { ScaffoldReactAppParams } from "../models/scaffold_react_app_params";
 
 const scaffoldReactApp = (params: ScaffoldReactAppParams) => {
   return new Promise<String>((resolve, reject) => {
+    
     const chp: ChildProcess = exec(
       `npx create-react-app ${params.applicationName} ${!params.js? '--template typescript': ""}`
     );
-    chp.stdout!.on("data", (msg: Buffer) => {
+    
+    chp.stdout?.on("data", (msg: Buffer) => {
       console.log(msg.toString());
     });
+    
+    // Error handling for exceptions
     chp.on('error', (err) => {
       reject(err);
     });
+
+    // Error handling for exceptions
+    chp.stderr?.on("data", (err) => {
+      console.log(err.toString());
+    });
+
+    
     chp.addListener("exit", (code) => {
       resolve("Done");
     });
