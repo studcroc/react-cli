e921f3281ceb22440c6667578c237110866c19b2
Harsh Vardhan Gautam <hvgautam24.vnit@gmail.com>
2021-06-27 13:52:15 +0530

    UPDATE template file to support both ts & js syntax

--- src/templates/rc-ts-template.ts
+++ src/templates/rc-ts-template.ts
@@ -1,31 +1,10 @@
 import { RCTParams } from "../models/rct_params";
 
 export function getRCTSTemplate(params: RCTParams) {
-
-    if(params.flags.css){
-        return `import React from 'react';
-import './${params.command.args[0]}.css';
-
-type ${params.command.args[0]}Props = {
-
-}
-
-export const ${params.command.args[0]} = (props: ${params.command.args[0]}Props) => {
+  return `import React from 'react';${params.flags.css ? `\nimport './${params.command.args[0]}.css';` : ""}${!params.flags.js ? `\ntype ${params.command.args[0]}Props = {\n\n}`: ""}
+\nexport const ${params.command.args[0]} = (props${!params.flags.js? `: ${params.command.args[0]}Props`: ""}) => {
     return (
         <></>
     );
 }`;
-    }else {
-        return `import React from 'react';
-
-type ${params.command.args[0]}Props = {
-
-}
-
-export const ${params.command.args[0]} = (props: ${params.command.args[0]}Props) => {
-    return (
-        <></>
-    );
-}`;
-    }
 }
